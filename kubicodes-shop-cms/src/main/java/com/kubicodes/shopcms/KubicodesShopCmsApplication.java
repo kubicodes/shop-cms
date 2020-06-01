ffaed8bd34e219f02692fbcbb1047af65bc4a59d
kubi <kubi@kubilays-mbp>
2020-06-01 14:33:49 +0200

    First Commit

---
+++ kubicodes-shop-cms/src/main/java/com/kubicodes/shopcms/KubicodesShopCmsApplication.java
@@ -0,0 +1,13 @@
+package com.kubicodes.shopcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubicodesShopCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubicodesShopCmsApplication.class, args);
+	}
+
+}
---
+++ kubicodes-shop-cms/src/test/java/com/kubicodes/shopcms/KubicodesShopCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubicodes.shopcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubicodesShopCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
