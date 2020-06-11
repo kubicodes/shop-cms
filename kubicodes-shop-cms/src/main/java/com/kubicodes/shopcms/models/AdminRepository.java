dac2344e0cedf71c7e580101094111bea824f991
kubi <kubi@kubilays-mbp>
2020-06-11 15:25:33 +0200

    Created Admin and User Repository

---
+++ kubicodes-shop-cms/src/main/java/com/kubicodes/shopcms/models/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.kubicodes.shopcms.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kubicodes.shopcms.models.data.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+}
